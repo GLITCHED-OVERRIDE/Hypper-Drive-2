d068332068e6628ac8fe8514b51769130aec1ecd
NOTAHACKER9999 <th15isar0bothi@gmail.com>
2026-01-01 15:05:30 -0600

    Update game.js

--- Games/Files/Source/PC/Balatro/game.js
+++ Games/Files/Source/PC/Balatro/game.js
@@ -19,8 +19,8 @@ Module.expectedDataFileDownloads++;
     } else {
       throw 'using preloaded data can only be done on a web page or in a web worker';
     }
-    var PACKAGE_NAME = 'game.data?v=d7e34743-2fea-4de6-8a0e-1103b5fcf07f';
-    var REMOTE_PACKAGE_BASE = 'game.data?v=d7e34743-2fea-4de6-8a0e-1103b5fcf07f';
+    var PACKAGE_NAME = 'https://github.com/GLITCHED-OVERRIDE/Hypper-Drive-2/raw/refs/heads/main/Games/Files/Source/PC/Balatro/game.data';
+    var REMOTE_PACKAGE_BASE = 'https://github.com/GLITCHED-OVERRIDE/Hypper-Drive-2/raw/refs/heads/main/Games/Files/Source/PC/Balatro/game.data';
     if (typeof Module['locateFilePackage'] === 'function' && !Module['locateFile']) {
       Module['locateFile'] = Module['locateFilePackage'];
       Module.printErr('warning: you defined Module.locateFilePackage, that has been renamed to Module.locateFile (using your locateFilePackage for now)');
@@ -284,5 +284,6 @@ Module.expectedDataFileDownloads++;
 
   }
   loadPackage({"package_uuid":"d7e34743-2fea-4de6-8a0e-1103b5fcf07f","remote_package_size":10642513,"files":[{"filename":"/game.love","crunched":0,"start":0,"end":10642513,"audio":false}]});
+
 
 })();
